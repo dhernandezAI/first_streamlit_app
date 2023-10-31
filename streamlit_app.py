60f4e81497b38d5f7f239d79e730ba343ce3c9d7
dhernandezAI <94905076+dhernandezAI@users.noreply.github.com>
2023-10-31 15:40:06 +0100

    Update streamlit_app.py
    Adding Favorites, and new header for Your own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🍓🥤 Perfect Strawberry Smoothie')
